fffceffc34bbf250f0332907c0b297b2f98c57eb
seapea2611 <seapea2002@gmail.com>
2024-01-31 02:18:37 +0700

    sua lai logic dan

--- Assets/Scripts/InteractionManager.cs
+++ Assets/Scripts/InteractionManager.cs
@@ -47,15 +47,16 @@ public class InteractionManager : MonoBehaviour
                             {
                                 // kiem tra xem thung dan hien tai co phai la dan cua pistol va hien tai dang cam khau pistol hay k
                                 case AmmoBox.AmmoType.PistolAmmo:
-                                    activeGun._currentBullets += hoverAmmoBox.ammoPistolAmount;
+                                    WeaponController.Instance.weapons[2]._currentBullets += hoverAmmoBox.ammoPistolAmount;
                                     Destroy(objectHitRaycast.gameObject);
+
                                     break;
                                 case AmmoBox.AmmoType.ShotGunAmmo:
-                                    activeGun._currentBullets += hoverAmmoBox.ammoShotGunAmount;
+                                    WeaponController.Instance.weapons[1]._currentBullets += hoverAmmoBox.ammoShotGunAmount;
                                     Destroy(objectHitRaycast.gameObject);
                                     break;
                                 case AmmoBox.AmmoType.AutoAmmo:
-                                    activeGun._currentBullets += hoverAmmoBox.ammoAutoAmount;
+                                    WeaponController.Instance.weapons[0]._currentBullets += hoverAmmoBox.ammoAutoAmount;
                                     Destroy(objectHitRaycast.gameObject);
                                     break;
                                 case AmmoBox.AmmoType.All:
